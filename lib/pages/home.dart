195e5db95e5e233715b388148ca29500022d28d4
Hwang2212 <hwang12616@gmail.com>
2022-07-24 00:05:46 +0800

    Tried fixing api again

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
-import 'package:dococr/services/data.dart';
+import 'package:dococr/api_service.dart';
+import 'package:dococr/model/customer_model.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:dococr/pages/profile.dart';
@@ -7,51 +8,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 // import 'package:dococr/services/data.dart';
 
-class Customer {
-  final int id;
-  final DateTime created_at;
-  final String customer_name;
-  final int ic;
-  final int age;
-  final String gender;
-  final String email;
-  final int phone_number;
-
-  Customer(
-    this.id,
-    this.created_at,
-    this.customer_name,
-    this.ic,
-    this.age,
-    this.gender,
-    this.email,
-    this.phone_number,
-  );
-}
-
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({Key? key, this.model}) : super(key: key);
+
+  final CustomerModel? model;
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  Future fetchAllCustomers() async {
-    var response = await http.get(Uri.parse('http://localhost:5000/customer'));
-    var jsonData = jsonDecode(response.body);
-    print(jsonData);
-    List<Customer> customers = [];
-
-    for (var c in jsonData) {
-      Customer customer = Customer(c['id'], c['created_at'], c['customer_name'],
-          c['ic'], c['age'], c['gender'], c['email'], c['phone_number']);
-      customers.add(customer);
-    }
-    print(customers.length);
-    return customers;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,7 +31,7 @@ class _HomeState extends State<Home> {
         body: Container(
           child: Card(
             child: FutureBuilder(
-                future: fetchAllCustomers(),
+                future: ,
                 builder: (context, AsyncSnapshot snapshot) {
                   // if (snapshot.data == null) {
                   //   return Container(
@@ -134,5 +100,20 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.lightBlueAccent,
           child: const Icon(Icons.person_add),
         ));
+  }
+
+  Widget loadCustomers(){
+    return FutureBuilder(
+      future: APIService.getCustomers(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<List<CustomerModel>?> model,
+      ) {
+         if (model.hasData){
+          return 
+         }
+      },
+
+      );
   }
 }
