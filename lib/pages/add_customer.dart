4e948bfd29849bd59f524939e62b8e573806bdd5
Hwang2212 <hwang12616@gmail.com>
2022-07-27 02:45:38 +0800

    Creating Customer Forms

--- lib/pages/add_customer.dart
+++ lib/pages/add_customer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
 
 class AddCustomer extends StatefulWidget {
   const AddCustomer({Key? key}) : super(key: key);
@@ -8,11 +10,44 @@ class AddCustomer extends StatefulWidget {
 }
 
 class _AddCustomerState extends State<AddCustomer> {
+  static final GlobalKey<FormState> globalKey = GlobalKey<FormState>();
+  bool isAPICallProcess = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    )
-    
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ProgressHUD(
+        child: Form(
+          key: globalKey,
+          child: Text(""),
+        ),
+        inAsyncCall: isAPICallProcess,
+        opacity: .3,
+        key: UniqueKey(),
+      ),
+    );
+  }
+
+  Widget customerForm() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10, top: 10),
+            child: FormHelper.inputFieldWidget(context, "Customer",
+                const Icon(Icons.ac_unit), onValidate, onSaved),
+          )
+        ],
+      ),
+    );
   }
 }
