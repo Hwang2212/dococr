df0b9970a4a118c69802c4af89bd959944f1db6b
Hwang2212 <andyhwangsauyee@gmail.com>
2022-08-22 01:11:21 +0800

    Changed to NodeJS Server deployed on Heroku

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,8 @@
 class Config {
   static const String appName = "Doc OCR";
   // 10.0.2.2 instead of 127.0.0.1 because it is a
-  static const String apiURL = "10.0.2.2:5000";
+  // static const String apiURL = "10.0.2.2:5000";
+  static const String apiURL = "dococrbackend.herokuapp.com";
   static const String customerURL = "/customer";
   static const String healthURL = "/health";
   static const String underwriteURL = "/underwrite";
